148abb3f49cdc1c9f5b25f6cca70186ac0dc7a84
Chris Reed <chrisreed87@gmail.com>
2022-06-02 17:00:15 -0700

    test sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt-get update && apt-get install \
+sudo apt-get update && sudo apt-get install \
   curl \
   git
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
+sudo git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
 echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
 echo ". $HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
